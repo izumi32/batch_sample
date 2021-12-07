2196d543a887c7d92ef70f58b6f7057981e92e31
izumi32 <k.izumi721@gmail.com>
2021-12-08 01:57:24 +0900

    create test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,17 @@
 Rank.delete_all
 User.delete_all
 
-user1 = User.create(id: 1, name: "aaa")
-user2 = User.create(id: 2, name: "bbb")
-user3 = User.create(id: 3, name: "ccc")
-user4 = User.create(id: 4, name: "ddd")
-user5 = User.create(id: 5, name: "eee")
+user_amount = ENV['USER_AMOUNT'].to_i
 
-UserScore.create(user_id: user1.id, score: 3, received_at: Time.current)
-UserScore.create(user_id: user1.id, score: 4, received_at: Time.current)
-UserScore.create(user_id: user1.id, score: 1, received_at: Time.current)
-
-UserScore.create(user_id: user2.id, score: 2, received_at: Time.current)
-UserScore.create(user_id: user2.id, score: 2, received_at: Time.current)
-UserScore.create(user_id: user2.id, score: 4, received_at: Time.current)
-
-UserScore.create(user_id: user3.id, score: 1, received_at: Time.current)
-UserScore.create(user_id: user3.id, score: 1, received_at: Time.current)
-UserScore.create(user_id: user3.id, score: 1, received_at: Time.current)
-
-UserScore.create(user_id: user4.id, score: 0, received_at: Time.current)
-UserScore.create(user_id: user4.id, score: 1, received_at: Time.current)
-UserScore.create(user_id: user4.id, score: 0, received_at: Time.current)
-
-UserScore.create(user_id: user5.id, score: 3, received_at: Time.current)
-UserScore.create(user_id: user5.id, score: 3, received_at: Time.current)
-UserScore.create(user_id: user5.id, score: 3, received_at: Time.current)
+User.transaction do
+  1.upto(user_amount) do |i|
+    user = User.create(id: i, name: "#{i}人目のユーザー")
+    rand(30).times do
+      UserScore.create(
+        user_id: user.id,
+        score: rand(1..100),
+        received_at: Time.current.ago(rand(0..60).days)
+      )
+    end
+  end
+end
