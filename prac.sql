1c280cfe05482f1e992fc0a0505d7bb82c8d6a93
izumi32 <k.izumi721@gmail.com>
2022-01-02 23:43:10 +0900

    sql ch4

--- prac.sql
+++ prac.sql
@@ -1,7 +1,3 @@
-SELECT emp_name,
-  CASE WHEN COUNT(*) = 1 THEN MAX(team)
-        WHEN COUNT(*) = 2 THEN '2つを兼務'
-        WHEN COUNT(*) >= 3 THEN '3つ以上を兼務'
-  END AS team
-FROM Employees
-GROUP BY emp_id;
+SELECT SUBSTRING(name, 1, 1) AS label, COUNT(*)
+FROM Persons
+GROUP BY label;
