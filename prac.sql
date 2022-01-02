36ba5db291bf8a4a09464abe9c7acde069aee509
izumi32 <k.izumi721@gmail.com>
2022-01-02 18:05:12 +0900

    sql ch3.2

--- prac.sql
+++ prac.sql
@@ -1,6 +1,7 @@
-SELECT name,
-  sex,
-  age,
-  rank() OVER (PARTITION BY sex ORDER BY age DESC) AS rnk
-FROM address
-;
+SELECT emp_name,
+  CASE WHEN COUNT(*) = 1 THEN MAX(team)
+        WHEN COUNT(*) = 2 THEN '2つを兼務'
+        WHEN COUNT(*) >= 3 THEN '3つ以上を兼務'
+  END AS team
+FROM Employees
+GROUP BY emp_id;
