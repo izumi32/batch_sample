773a69baf8e3ca09ecb2c7f14d73f4e6b43be288
izumi32 <k.izumi721@gmail.com>
2022-01-05 16:26:54 +0900

    sql ch6.1

--- prac.sql
+++ prac.sql
@@ -1,3 +1,11 @@
-SELECT name, age,
-  RANK() OVER (ORDER BY age)
-FROM Address;
+SELECT E.emp_id, E.emp_name, E.dept_id, D.dept_name
+FROM Employees E INNER JOIN Departments D
+ON E.dept_id = D.dept_id;
+
+SELECT E.emp_id, E.emp_name, E.dept_id, D.dept_name
+FROM Departments D LEFT OUTER JOIN Employees E
+ON E.dept_id = D.dept_id;
+
+SELECT D1.digit * 10 + D2.digit AS num
+FROM Digits D1 CROSS JOIN Digits D2
+ORDER BY D1.digit, D2.digit;
